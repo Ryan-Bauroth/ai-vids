defb3c2ab7b25368e0d5bc7a38f81136aa0cf7f2
Ryan Bauroth <25bauroth@da.org>
2024-06-14 16:14:21 -0400

    initial commit

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,14 +1,6 @@
-import { spring } from "remotion";
 import {
-  AbsoluteFill,
-  interpolate,
-  Sequence,
-  useCurrentFrame,
-  useVideoConfig,
+  AbsoluteFill, staticFile, Video, Audio
 } from "remotion";
-import { Logo } from "./HelloWorld/Logo";
-import { Subtitle } from "./HelloWorld/Subtitle";
-import { Title } from "./HelloWorld/Title";
 import { z } from "zod";
 import { zColor } from "@remotion/zod-types";
 
@@ -25,52 +17,13 @@ export const HelloWorld: React.FC<z.infer<typeof myCompSchema>> = ({
   logoColor1,
   logoColor2,
 }) => {
-  const frame = useCurrentFrame();
-  const { durationInFrames, fps } = useVideoConfig();
 
-  // Animate from 0 to 1 after 25 frames
-  const logoTranslationProgress = spring({
-    frame: frame - 25,
-    fps,
-    config: {
-      damping: 100,
-    },
-  });
-
-  // Move the logo up by 150 pixels once the transition starts
-  const logoTranslation = interpolate(
-    logoTranslationProgress,
-    [0, 1],
-    [0, -150],
-  );
-
-  // Fade out the animation at the end
-  const opacity = interpolate(
-    frame,
-    [durationInFrames - 25, durationInFrames - 15],
-    [1, 0],
-    {
-      extrapolateLeft: "clamp",
-      extrapolateRight: "clamp",
-    },
-  );
-
-  // A <AbsoluteFill> is just a absolutely positioned <div>!
+// A <AbsoluteFill> is just a absolutely positioned <div>!
   return (
-    <AbsoluteFill style={{ backgroundColor: "white" }}>
-      <AbsoluteFill style={{ opacity }}>
-        <AbsoluteFill style={{ transform: `translateY(${logoTranslation}px)` }}>
-          <Logo logoColor1={logoColor1} logoColor2={logoColor2} />
-        </AbsoluteFill>
-        {/* Sequences can shift the time for its children! */}
-        <Sequence from={35}>
-          <Title titleText={propOne} titleColor={propTwo} />
-        </Sequence>
-        {/* The subtitle will only enter on the 75th frame. */}
-        <Sequence from={75}>
-          <Subtitle />
-        </Sequence>
+      <AbsoluteFill style={{backgroundColor: "black"}}>
+        <Audio src={staticFile("song.mp3")} />
+        <p style={({backgroundColor: "black", color: "white", fontSize: "48px"})}>INSERT CAPTION HERE</p>
+        <Video src={staticFile("vid.mp4")}/>
       </AbsoluteFill>
-    </AbsoluteFill>
   );
 };
